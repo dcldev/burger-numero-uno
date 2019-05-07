4b36d9c0cba88722361096f3efc27ef91d399264
Han Seoulo <cu.rogue.coder@gmail.com>
2019-05-07 18:21:38 -0400

    initialized files and began coding handlebars referencing CATS class assignment

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Referenced CATS class exercise for the correct inline insert commands for SQL
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stalone BURGER', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mason BURGER', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Biden Touch BURGER', true);
+
---
+++ models/burger.js
@@ -0,0 +1,33 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+// establishing varible to create model functions to interact with SQL database using CRUD
+
+// by requiring ORM we can reference the ORM functions created to manipulate the SQL database.
+
+let burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
