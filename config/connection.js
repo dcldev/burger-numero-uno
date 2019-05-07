d44e3ab2a4928df349fe5682d7709a0334c52def
Han Seoulo <cu.rogue.coder@gmail.com>
2019-05-07 19:54:49 -0400

    adjusted files for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,23 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
+
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
